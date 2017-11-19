d7772704ba091dc0ef784104d51b99c7bb2a56da
Matyas Yves Constans <mconstanshu@gmail.com>
2017-11-19 10:51:48 +0100

    Update FunctionDictionary.cs

--- KelpNet/Common/Functions/Container/FunctionDictionary.cs
+++ KelpNet/Common/Functions/Container/FunctionDictionary.cs
@@ -11,39 +11,37 @@ namespace KelpNet.Common.Functions.Container
     {
         const string FUNCTION_NAME = "FunctionDictionary";
 
-        //関数に入出力のキーを付加したFunctionRecordという単位で管理する
+        // Managed with FunctionRecord, input/output key added to the function.
         public Dictionary<string, FunctionStack> FunctionBlockDictionary = new Dictionary<string, FunctionStack>();
 
-        //分割関数の名前を保持する辞書
+        // Dictionary holding names of division functions
         public Dictionary<string, FunctionStack> SplitedFunctionDictionary = new Dictionary<string, FunctionStack>();
 
-        //辞書の実行順リスト
+        // Dictionary execution order list
         public List<FunctionStack> FunctionBlocks = new List<FunctionStack>();
 
         private readonly bool _compress = false;
 
-        //コンストラクタ
         public FunctionDictionary(bool compress = false, string name = FUNCTION_NAME, string[] inputNames = null, string[] outputNames = null) : base(name, inputNames, outputNames)
         {
             this._compress = compress;
         }
 
-        //関数の追加
         public void Add(Function function)
         {
-            if (_compress && //分岐毎のまとめを行うか
-                (function is SingleInputFunction || function is MultiOutputFunction)) //入力が一つの関数のみまとめられる
+            if (_compress && // Summarize each branch or not
+                (function is SingleInputFunction || function is MultiOutputFunction)) // Gather only on function
             {
-                //入力名称で辞書に登録が有るかチェック
+                // Check if it was registered in the dict
                 if (this.FunctionBlockDictionary.ContainsKey(function.InputNames[0]))
                 {
-                    //ブロックが既に辞書登録されていればブロックに連結
+                    // If the block has already been registered, it is concatenated to the block
                     this.FunctionBlockDictionary[function.InputNames[0]].Add(function);
 
-                    //出力名称を上書き
+                    // Overwrite output name
                     this.FunctionBlockDictionary[function.InputNames[0]].OutputNames = function.OutputNames.ToArray();
 
-                    //分割済み関数なら分割元の出力名を更新する
+                    // If it is a divided function update the output name of the source
                     if (SplitedFunctionDictionary.ContainsKey(function.InputNames[0]))
                     {
                         FunctionStack spliteFunction = SplitedFunctionDictionary[function.InputNames[0]];
@@ -62,22 +60,22 @@ namespace KelpNet.Common.Functions.Container
                         }
                     }
 
-                    if (!(function is MultiOutputFunction) && //出力が分岐する場合は登録せずリンクを切る
-                      !this.FunctionBlockDictionary.ContainsKey(function.OutputNames[0])) //既に登録されている場合は登録しない
+                    if (!(function is MultiOutputFunction) && // If the output branches, do not register and cut the link
+                      !this.FunctionBlockDictionary.ContainsKey(function.OutputNames[0])) // Do not register if already registered
                     {
-                        //リンクを辞書へ追加
+                        // Add link to dictionary
                         this.FunctionBlockDictionary.Add(function.OutputNames[0], this.FunctionBlockDictionary[function.InputNames[0]]);
                     }
-                    else if (function is SplitFunction) //SplitFunctionの場合
+                    else if (function is SplitFunction) // SplitFunction
                     {
                         var splitFunctions = ((SplitFunction)function).SplitedFunctions;
 
                         for (int i = 0; i < splitFunctions.Length; i++)
                         {
-                            //内部のFunctionStackをリンクの辞書へ追加
+                            // Add internal FunctionStack to link dictionary
                             FunctionBlockDictionary.Add(function.OutputNames[i], splitFunctions[i]);
 
-                            //SplitFunctionのリストへ追加
+                            // Add to SplitFunction's list
                             SplitedFunctionDictionary.Add(function.OutputNames[i], this.FunctionBlockDictionary[function.InputNames[0]]);
                         }
                     }
@@ -86,23 +84,23 @@ namespace KelpNet.Common.Functions.Container
                 }
             }
 
-            //以下無圧縮、もしくはMultiInput,DualInput用の処理
+            // Processing for uncompressed, or MultiInput, DualInput below
 
-            //ブロックが辞書に登録されているか
+            // Check if block is registered in the dictionary
             if (this.FunctionBlockDictionary.ContainsKey(function.OutputNames[0]))
             {
-                //ブロックが既に辞書登録されていればブロックに連結
+                // If the block has already been dictionary registered, it is concatenated to the block
                 this.FunctionBlockDictionary[function.OutputNames[0]].Add(function);
             }
             else
             {
-                //登録がなければブロックを新規に作る
+                // If it wasn't registered create a new block
                 FunctionStack functionRecord = new FunctionStack(function, function.Name, function.InputNames, function.OutputNames);
 
-                //実行順に登録
+                // Register in order of execution
                 this.FunctionBlocks.Add(functionRecord);
 
-                //リンクを辞書へ追加
+                //  Add link to dictionary
                 this.FunctionBlockDictionary.Add(function.Name, functionRecord);
             }
         }
@@ -112,31 +110,31 @@ namespace KelpNet.Common.Functions.Container
         {
             NdArray[] result = xs;
 
-            //出力データの辞書
+            // Dictionary of output data
             Dictionary<string, NdArray> outPuts = new Dictionary<string, NdArray>();
 
-            //最初のデータを辞書に登録
+            // Register the first data in the dictionary
             for (int i = 0; i < FunctionBlocks[0].InputNames.Length; i++)
             {
                 outPuts.Add(FunctionBlocks[0].InputNames[i], xs[i]);
             }
 
-            //登録順で実行していく
+            // Run in order of registration
             for (int i = 0; i < FunctionBlocks.Count; i++)
             {
                 string[] inputBlockNames = FunctionBlocks[i].InputNames;
                 List<NdArray> inputData = new List<NdArray>();
 
-                //入力するデータを集めてくる
+                // Collect the input data
                 for (int j = 0; j < inputBlockNames.Length; j++)
                 {
                     inputData.Add(outPuts[inputBlockNames[j]]);
                 }
 
-                //関数を実施
+                // Execute function
                 result = FunctionBlocks[i].Forward(inputData.ToArray());
 
-                //出力したデータを辞書に登録
+                // Register the outputted data in the dictionary
                 for (int j = 0; j < result.Length; j++)
                 {
                     outPuts.Add(FunctionBlocks[i].OutputNames[j], result[j]);
@@ -152,7 +150,7 @@ namespace KelpNet.Common.Functions.Container
             NdArray.Backward(ys[0]);
         }
 
-        //重みの更新処理
+        // Weight update process
         public override void Update()
         {
             foreach (var functionBlock in FunctionBlocks)
@@ -161,7 +159,7 @@ namespace KelpNet.Common.Functions.Container
             }
         }
 
-        //ある処理実行後に特定のデータを初期値に戻す処理
+        // Return specific data to the initial value after a certain process is executed
         public override void ResetState()
         {
             foreach (var functionBlock in FunctionBlocks)
@@ -170,15 +168,15 @@ namespace KelpNet.Common.Functions.Container
             }
         }
 
-        //予想を実行する
+        // Execute forecast
         public override NdArray[] Predict(params NdArray[] xs)
         {
             NdArray[] result = xs;
 
-            //出力データの辞書
+            // Dictionary of output data
             Dictionary<string, NdArray> outPuts = new Dictionary<string, NdArray>();
 
-            //出力したデータを辞書に登録
+            // Register the output data in the dictionary
             for (int j = 0; j < FunctionBlocks[0].InputNames.Length; j++)
             {
                 outPuts.Add(FunctionBlocks[0].InputNames[j], xs[j]);
@@ -189,16 +187,16 @@ namespace KelpNet.Common.Functions.Container
                 string[] inputBlockNames = FunctionBlocks[i].InputNames;
                 List<NdArray> inputData = new List<NdArray>();
 
-                //入力するデータを集めてくる
+                // Collect input data
                 for (int j = 0; j < inputBlockNames.Length; j++)
                 {
                     inputData.Add(outPuts[inputBlockNames[j]]);
                 }
 
-                //関数を実施
+                // Execute function
                 result = FunctionBlocks[i].Predict(inputData.ToArray());
 
-                //出力したデータを辞書に登録
+                // Register the output data in the dictionary
                 for (int j = 0; j < result.Length; j++)
                 {
                     outPuts.Add(FunctionBlocks[i].OutputNames[j], result[j]);
